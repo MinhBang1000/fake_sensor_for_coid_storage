5e1a5e3262d5f316bf160d214c8eedec87d48045
Minh Bang <bradonleminhbang1@gmail.com>
2022-10-27 13:38:15 +0700

    SECRET KEY get env

--- fake_sensor/settings.py
+++ fake_sensor/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y+ls0hyz*y_sknus-b$s%gr=bf0&gk%^k-fs_+c$7(0hk8^94w'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
